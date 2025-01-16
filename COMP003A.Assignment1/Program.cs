0a99b6f45fe4e068a85d78d6b90114c86d1b5c6f
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-01-16 00:57:28 -0800

    Changed the name from "Jonathan Cruz" to "Riley Benson" in the greeting and filled the bullet points in up to an empty bullet point #6.
    These changes apply to the file "Program.cs"

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -17,7 +17,7 @@ namespace COMP003.LectureActivity1
             // Display a single message
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + "! My name is Jonathan Cruz.\n\n\nHere are 10 things about me:\n\t1. I have started using computers at the young age of 4 years old.\n\t2. ");
+            Console.WriteLine("Hello, " + name + "! My name is Riley Benson.\n\n\nHere are 10 things about me:\n\t1. I have started using computers at the young age of 4 years old.\n\t2. My first computer OS I used was Windows 7.\n\t3. I used to be a Duck Life 1 and 3 speedrunner (unofficially).\n\t4. I prefer Android over iOS and Linux over Windows and Mac.\n\t5. I play Super Smash Bros. somewhat competitively, and have even attneded a few tournaments, including one in-person one.\n\t6. ");
         }
     }
 }
